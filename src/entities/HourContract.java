74bf8f6be6ca6c914011e02d67fb4df51e8888e3
vitorPatrick <vitor_patrick@yahoo.com>
2021-02-19 16:13:59 -0300

    Class Worker finalziada

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -1,5 +1,45 @@
 package entities;
 
-public class HourContract {
+import java.util.Date;
 
+public class HourContract {
+	
+	private Date date;
+	private Double valueperHour;
+	private Integer hours;
+	
+	public HourContract() {
+	}
+
+	public HourContract(Date date, Double valueperHour, Integer hours) {
+		super();
+		this.date = date;
+		this.valueperHour = valueperHour;
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValueperHour() {
+		return valueperHour;
+	}
+	public void setValueperHour(Double valueperHour) {
+		this.valueperHour = valueperHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	
+	public double totalValue() {
+		return valueperHour * hours;
+	}
 }
